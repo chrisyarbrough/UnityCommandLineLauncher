9794cc328387842d0be28495e69c4f373c6f31ce
Chris Yarbrough <17833862+chrisyarbrough@users.noreply.github.com>
2024-02-08 15:01:38 +0100

    Log errors to stderror and colorize in terminals

--- open-unity.py
+++ open-unity.py
@@ -7,17 +7,6 @@ import subprocess
 from pathlib import Path
 
 
-def find_version(project_settings_path):
-    # Extract the Unity version from the ProjectSettings file.
-    version_pattern = re.compile(r'm_EditorVersion: (.*)')
-    with open(project_settings_path, 'r') as file:
-        for line in file:
-            match = version_pattern.match(line)
-            if match:
-                return match.group(1)
-    return None
-
-
 def main():
     # Starting from the current working directory, search in multiple locations for the Unity ProjectVersion file.
     current_directory = Path.cwd()
@@ -37,20 +26,17 @@ def main():
 
     if not project_settings_path:
         paths_string = '\n'.join(str(path.resolve()) for path in project_settings_paths)
-        print(f"Couldn't find ProjectSettings file in:\n{paths_string}")
-        sys.exit(1)
+        exit_with_error(f"Couldn't find ProjectSettings file in:\n{paths_string}", 1)
 
     unity_version = find_version(project_settings_path)
 
     if unity_version is None:
-        print("Couldn't find Unity version in ProjectSettings file")
-        sys.exit(2)
+        exit_with_error(f"Couldn't find Unity version in ProjectSettings file", 2)
 
     unity_editor_path = f"/Applications/Unity/Hub/Editor/{unity_version}/Unity.app/Contents/MacOS/Unity"
 
     if not os.path.isfile(unity_editor_path):
-        print(f"Couldn't find Unity Editor installation at {unity_editor_path}")
-        sys.exit(3)
+        exit_with_error(f"Couldn't find Unity Editor installation at {unity_editor_path}", 3)
 
     # The project path is the one which contains the Assets, Library and ProjectSettings folders.
     project_path = str(project_settings_path.parent.parent)
@@ -70,6 +56,27 @@ def main():
 
     # Start a new process which continues to live even after the shell is closed.
     subprocess.Popen(command)
+
+
+def find_version(project_settings_path):
+    # Extract the Unity version from the ProjectSettings file.
+    version_pattern = re.compile(r'm_EditorVersion: (.*)')
+    with open(project_settings_path, 'r') as file:
+        for line in file:
+            match = version_pattern.match(line)
+            if match:
+                return match.group(1)
+    return None
+
+
+def exit_with_error(message: str, code: int):
+    if sys.stderr.isatty():
+        # Use ANSI colors in terminal output, but not when piping to a file.
+        red_start = "\033[31m"
+        reset = "\033[0m"
+        message = f"{red_start}{message}{reset}"
+    sys.stderr.write(message + '\n')
+    sys.exit(code)
 
 
 if __name__ == '__main__':
