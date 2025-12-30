6f43a0679c13a4b6929d1dc317fb8c744855c387
Chris Yarbrough <mail@chrisyarbrough.com>
2025-12-30 02:54:34 +0100

    Refactor

--- bash/get_unity_editor.sh
+++ bash/get_unity_editor.sh
@@ -6,18 +6,22 @@ if [ $# -lt 1 ]; then
 fi
 
 VERSION="$1"
-UNITY_HUB="$(mdfind "kMDItemCFBundleIdentifier == 'com.unity3d.unityhub'" | head -n 1)/Contents/MacOS/Unity Hub"
+source "$(dirname "$0")/shared.sh"
+UNITY_HUB=$(get_unity_hub)
 
-"$UNITY_HUB" -- --headless editors --installed 2>/dev/null | while IFS= read -r line; do
-    # (\d+\.\d+\.\d+[abfp]\d+)\s+.*installed at (.+)
+while IFS= read -r line; do
     if [[ "$line" =~ ([0-9]+\.[0-9]+\.[0-9]+[a-z0-9]*)[[:space:]]+\(.*\)[[:space:]]+installed[[:space:]]+at[[:space:]]+(.+) ]]; then
         LINE_VERSION="${BASH_REMATCH[1]}"
         LINE_PATH="${BASH_REMATCH[2]}"
       
         if [[ "$LINE_VERSION" == "$VERSION" ]]; then
             echo "$LINE_PATH"
+            exit 0
         fi
     else
         echo "Warning: Could not parse line: $line" >&2
     fi
-done
+done <<< "$("$UNITY_HUB" -- --headless editors --installed 2>/dev/null)"
+
+echo "Error: Unity version $VERSION is not installed." >&2
+exit 1
