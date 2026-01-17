187160cd09a4c070eed927d9f2cb01b787135822
Chris Yarbrough <17833862+chrisyarbrough@users.noreply.github.com>
2026-01-17 02:42:14 +0100

    Remove bash, go and python implementations

--- bash/utility.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-get_unity_hub() {
-  echo "$(mdfind "kMDItemCFBundleIdentifier == 'com.unity3d.unityhub'" | head -n 1)/Contents/MacOS/Unity Hub"
-}
--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/chrisyarbrough/unity-launcher
-
-go 1.25.5
