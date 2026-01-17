b5e432903771512af1fe8dd0a981a4b1b25b639b
Chris Yarbrough <17833862+chrisyarbrough@users.noreply.github.com>
2026-01-17 02:14:04 +0100

    Make PlatformSupportTest OSX only

--- dotnet/ucll.tests/PlatformSupportTests.cs
+++ dotnet/ucll.tests/PlatformSupportTests.cs
@@ -1,11 +1,25 @@
+using System.Runtime.InteropServices;
+
 public class PlatformSupportTests
 {
-	[Fact]
+	[Fact_PlatformOSX]
 	public void FindInstallationRootReturnsValidRoot()
 	{
 		PlatformSupport platformSupport = PlatformSupport.Create();
 		const string editorPath = "/Applications/Unity/Hub/Editor/2022.3.10f1/Unity.app/Contents/MacOS/Unity";
 		string root = platformSupport.FindInstallationRoot(editorPath);
 		Assert.Equal("/Applications/Unity/Hub/Editor/2022.3.10f1", root);
+	}
+
+	// ReSharper disable once InconsistentNaming
+	private sealed class Fact_PlatformOSX : FactAttribute
+	{
+		public Fact_PlatformOSX()
+		{
+			if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				Skip = "Test only runs on macOS";
+			}
+		}
 	}
 }
